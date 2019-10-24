549c54d914f620dc48d4170b706243ccb54f7597
Ziyat <termit_90@mail.ru>
2019-10-24 10:44:21 +0500

    fix

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -5,7 +5,6 @@ use yii\helpers\Html;
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-
 if (Yii::$app->controller->action->id === 'login') {
     /**
      * Do not use this code in your template. Remove it.
