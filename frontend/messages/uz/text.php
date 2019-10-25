a48dbc1d42d44c8ec314409ae4a1af7f2958aa43
Ziyat <termit_90@mail.ru>
2019-10-25 19:58:51 +0500

    fixed

---
+++ frontend/messages/uz/text.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'header_promo-title' => '"SHUXRAT YOG\'DUSI" tarmoqli tashkiloti',
+    'header_join-title' => 'O\'zbekiston ishlab chiqaruvchilarining maxsulotlarini targ\'ibot (reklama) qilish bilan faoliyat yuritadi.',
+    'section_1_hello' => 'Assalomu alaykum',
+    'section_1_question' => 'Savol: Nima uchun g\'oz tuxumiga qaraganda tovuq tuxumining bozori chaqqon bo\'ladi? Vaholanki tovuq tuxumiga qaraganda g\'oz tuxumi ham katta, ham kolloriyasi kuchliroq.',
+    'section_1_answer' => ' CHUNKI: Hamma gap reklamada!!!',
+    'section_1_text_1' => 'Tovuq tuxum qo\'yganida tuxum haqida butun olamga jar soladi. G\'oz tuxum qo\'yganini xatto bilmay qolamiz. Yo, noto\'g\'rimi?',
+    'section_1_text_2' => ' Shuning uchun bozori chaqqon tuxum - TOVUQNIKI',
+    'section_2_title' => 'Bizning "SHUXRAT YOG\'DUSI" tarmoqli tashkilotimiz',
+    'section_2_text_1' => 'O\'zbekistondagi ishlab chiqaruvchilarni qo\'llab-quvvatlash maqsadida ularning sifatli maxsulotlarini yakka tartibda targ\'ibot qilib har bir xonadonga olib kirishni va butun olamga maxsulotlarning sifati haqida jar solishni rejalashtirgan. Shu sabab tarmoqli tashkilot tuzildi. Tarmoq binar sistemasida ish yuritadi va tashkilotimizga a\'zo bo\'lganlar sotuvchi-agent lavozimiga ega bo\'lishadi. Tarmoq asosida targ\'ibot ishlari bilan shug\'ullangan targ\'ibotchi hamkorlarimizga KATTA imkoniyatlar yaratib beramiz, ya\'ni:',
+    'section_2_ul' => '<ul><li>Mehnat daftarchasi ochiladi.</li><li>INPS rasmiylashtiriladi.</li><li>Daromad plastik kartaga o\' tkaziladi.</li></ul>',
+    'section_2_mark' => 'Bizga hamkor bo\'lgan har bir shaxs korxona qo\'ygan talablarni o\'z vaqtida bajarsalar raxbariyat tomonidan sovg\'alar taqdim etiladi',
+    'section_2_text_2' => '<p>Tashkilotga a\'zo bo\'lganlar o\'zining kasbiy mahoratini oshirgan holda aholiga xizmat ko\'rsati</p><p>yaxshilash bilan bir qatorda aholiga muvaffaqiyatlarga erishishga imkon beradi.</p>',
+    'section_3_text_1' => 'Tashkilotning bosh maqsadi: aholiga uyidan chiqmay maishiy texnika va uy-ro\'zg\'or buyumlarga ega bo\'lishni ta\'minlash.',
+    'section_3_ul' => '<ul class="list-unstyled"><li><h5>Yashash uchun qulay sharoit yaratish.</h5></li><li><h5>Sog\'lom turmush tarzini shakllantirish.</h5></li><li><h5>Moliyaviy holatni yaxshilash.</h5></li></ul>',
+    'about_title' => 'Toshkent viloyati O\'rtachirchiq tumanida joylashgan " SHUXRAT YOG\'DUSI" tarmoqli tashkiloti.',
+    'about_text' => 'Ozgina tanaffusdan so\'ng 2019 yilda o\'z faoliyatini yangiladi. O\'zbekiston Respublikasi Vazirlar Mahkamasining 2012 yil 1 fevralidagi"O\'zbekiston Respublikasida chakana savdo qoidalariga go\'shimcha kiritishto\'g\'risida"gi 25-sonli Qarori asosida mamlakatning ishlab chiqaruvchilarini qo\'llab- quvvatlash maqsadida milliy maxsulotlarni yakka tartibda targ\'ibot qilib, har bir xonadonga olib kirib borishni rejalashtirgan.'
+];
