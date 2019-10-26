d8256eafee3a4b9af53dfd4c40fd69240222aeda
Ziyat <termit_90@mail.ru>
2019-10-26 17:14:57 +0500

    fix

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => 'fatam.uz',
-    'language' => 'ru',
+    'language' => 'uz',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
